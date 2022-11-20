615f47e610e1e2ef014dc1c4b7139cae9c472ae2
Michael Eggers <michael.d.eggers@gmail.com>
2022-11-19 22:57:26 -0500

    allow user to assume role

--- iam.tf
+++ iam.tf
@@ -3,6 +3,47 @@ resource "aws_iam_user" "projects" {
   name     = "${var.organization}-${each.key}-user"
 }
 
+resource "aws_iam_user_policy" "projects" {
+  for_each = var.projects
+  name     = "${var.organization}-${each.key}-assume-role-policy"
+  user     = aws_iam_user.projects[each.key].name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          "AWS" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user_policy" "eggs_cli" {
+  name = "eggs-cli-assume-role-policy"
+  user = "eggs-cli"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          "AWS" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_access_key" "projects" {
   for_each = var.projects
   user     = aws_iam_user.projects[each.key].name
