00459e06b10e50c3db5ee7e65330f6dd4deb3012
Michael Eggers <michael.d.eggers@gmail.com>
2022-11-25 22:36:30 -0500

    add enviornments to repos

---
+++ github.tf
@@ -0,0 +1,45 @@
+# Setup Repos for Projects
+resource "github_repository" "projects" {
+  for_each    = var.projects
+  name        = each.key
+  description = each.value["description"]
+
+  visibility = each.value["visibility"]
+
+  template {
+    owner                = each.value["template"]["owner"]
+    repository           = each.value["template"]["repo"]
+    include_all_branches = false
+  }
+}
+
+resource "github_actions_secret" "tf_api_token" {
+  for_each        = var.projects
+  repository      = github_repository.projects[each.key].name
+  secret_name     = "TF_API_TOKEN"
+  plaintext_value = var.team_api_token
+}
+
+resource "github_repository_environment" "dev" {
+  for_each    = local.projects_prod
+  environment = "dev"
+  repository  = github_repository.projects[each.key].name
+
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
+
+resource "github_repository_environment" "prod" {
+  for_each    = local.projects_prod
+  environment = "prod"
+  repository  = github_repository.projects[each.key].name
+  reviewers {
+    users = [data.github_user.current.id]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+}
--- main.tf
+++ main.tf
@@ -40,25 +40,3 @@ module "tfe_prod" {
   hosted_zone    = var.environments.prod.hosted_zone
   access_keys    = module.aws_prod.access_keys
 }
-
-# Setup Repos for Projects
-resource "github_repository" "projects" {
-  for_each    = var.projects
-  name        = each.key
-  description = each.value["description"]
-
-  visibility = each.value["visibility"]
-
-  template {
-    owner                = each.value["template"]["owner"]
-    repository           = each.value["template"]["repo"]
-    include_all_branches = false
-  }
-}
-
-resource "github_actions_secret" "tf_api_token" {
-  for_each        = var.projects
-  repository      = each.key
-  secret_name     = "TF_API_TOKEN"
-  plaintext_value = var.team_api_token
-}
